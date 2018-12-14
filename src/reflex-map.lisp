9f8b4dcf32c15a80ed95855938b2aa98b2ff80c7
Alexey Veretennikov <alexey.veretennikov@gmail.com>
2018-12-14 19:27:50 +0100

    Parser rewritten using cl-yacc

--- src/reflex-map.lisp
+++ src/reflex-map.lisp
@@ -13,25 +13,9 @@
 
 ;; Implementation
 
-(define-condition map-parse-error (error)
-  ((text :initarg :text))
-  (:report (lambda (condition stream)
-             (format stream "Parse file error: ~a" (slot-value condition 'text)))))
-
-(define-condition parse-header-error (map-parse-error) nil)
-
-(define-condition parse-entity-error (map-parse-error) nil)
-
-(define-condition parse-brush-error (map-parse-error) nil)
-
-(define-condition parse-vertices-error (parse-brush-error) nil)
-
-(define-condition parse-face-error (parse-brush-error) nil)
-
-
 (defclass reflex-map ()
-  ((entities :initform nil :reader map-entities)
-   (brushes :initform nil :reader map-brushes)))
+  ((entities :initarg :entities :initform nil :reader map-entities)
+   (brushes :initarg :brushes :initform nil :reader map-brushes)))
     
 
 (defclass entity ()
@@ -41,9 +25,9 @@
 
 
 (defclass brush ()
-  ((vertices :initform nil :reader brush-vertices
+  ((vertices :initarg :vertices :initform nil :reader brush-vertices
              :documentation "A list of vertices")
-   (faces :initform nil :reader brush-faces
+   (faces :initarg :faces :initform nil :reader brush-faces
           :documentation "A list of faces")))
 
 
@@ -66,10 +50,11 @@
 
 (defmethod print-object :after ((self entity) stream)
   "Print the contents of the ENTITY"
-  (with-slots (type properties) self
-    (format stream "Entity type: ~a~%Properties:~%" type)
-    (dolist (x properties)
-      (format stream "~a~%" x))))
+  nil)
+;;;   (with-slots (type properties) self
+;;;     (format stream "Entity type: ~a~%Properties:~%" type)
+;;;     (dolist (x properties)
+;;;       (format stream "~a~%" x))))
 
 (defmethod print-vertex ((self vertex) stream)
   "Print the contents of the VERTEX"
@@ -82,42 +67,17 @@
 
 (defmethod print-object :after ((self brush) stream)
   "Print the contents of the BRUSH"
-  (with-slots (vertices faces) self
-    (format stream "Bursh~%Vertices:~%")
-    (dolist (v vertices) (print-object v stream))
-    (format stream "Faces:~%")
-    (dolist (f faces) (print-object f stream))))
+  nil)
+;;;   (with-slots (vertices faces) self
+;;;     (format stream "Bursh~%Vertices:~%")
+;;;     (dolist (v vertices) (print-object v stream))
+;;;     (format stream "Faces:~%")
+;;;     (dolist (f faces) (print-object f stream))))
 
 
 (defun read-windows-line (in)
   (when-let (line (read-line in nil))
     (string-right-trim '(#\Newline #\Return) line)))
-
-
-(defun parse-header-line (in-stream)
-  "Parses the first (header) line and returns a file version"
-  (when-let (line (read-windows-line in-stream))
-    (destructuring-bind (game map version number)
-        (split-sequence #\Space line)
-      (let ((ver (parse-integer number)))
-        (when (and (string= game "reflex")
-                   (string= map "map")
-                   (string= version "version")
-                   (> ver 5))
-          ver)))))
-
-(defmacro def-if-header (header)
-  (let ((fun-name (concatenate 'string "if-" header)))
-    `(defun ,(intern (string-upcase fun-name)) (line)
-       (string=
-        (string-trim '(#\Tab #\Space) line)
-        ,header))))
-
-
-(def-if-header "brush")
-(def-if-header "entity")
-(def-if-header "vertices")
-(def-if-header "faces")
 
 
 (defun count-indent (line &optional (indent-char #\tab))
@@ -134,102 +94,228 @@
   (split-sequence:split-sequence #\Space (string-trim '(#\Tab #\Space) line)))
 
 
-(defmethod parse-entity ((self entity) in)
-  (with-slots (properties) self
-    ;; first line must be a type
-    (let ((first-line (read-windows-line in)))
-      (unless (if-indent first-line 1)
-        (error 'parse-entity-error :text "First entity line is not indented with 1 tab"))
-      (let ((parsed-first-line (parse-and-trim first-line)))
-        (unless (string= (car parsed-first-line) "type")
-          (error 'parse-entity-error :text "First entity line is not a 'type'"))
-        (setf (slot-value self 'type) (second parsed-first-line))))
-    (loop for line = (read-windows-line in )
-          while (if-indent line 1)
-          do 
-             (push (parse-and-trim line) properties)
-          finally
-             (setf properties (nreverse properties))
-             (return line))))
 
 
-(defun parse-vertex (line)
-  (destructuring-bind (x y z)
-      (mapcar #'read-from-string (parse-and-trim line))
-    (make-instance 'vertex :x x :y y :z z)))
 
-(defun parse-face (line)
-  ;; example:
-  ;; 0.000000 0.000000 1.000000 1.000000 0.000000 0 1 2 3 structural/dev/dev_nogrid_grey128
-  ;; faces format:
-  ;; offset u v, scale u v, rotation
-  (let* ((parsed (parse-and-trim line))
-         (texture-offsets (subseq parsed 0 5))
-         (texture-name (car (last parsed)))
-         (vertices (subseq parsed 5 (1- (length parsed)))))
-    (destructuring-bind (u v s-u s-v rot)
-        (mapcar #'read-from-string texture-offsets )
-      (make-instance 'face
-                     :u u :v v :scale-u s-u :scale-v s-v
-                     :rotation rot
-                     :texture-name texture-name
-                     :vertices
-                     (mapcar #'parse-integer
-                             vertices)))))
-                   
+
+;;; new implementation
+
+(defparameter *parsed-queue* nil)
+
+(defparameter *float-scanner*
+  (ppcre:create-scanner "-?[0-9]+([.][0-9]+([Ee][0-9]+)?)"))
+
+(defparameter *int-scanner*
+  (ppcre:create-scanner "-?[0-9]+"))
+
+
+(defun is-float (token)
+  "check if the full string is a full floating point number"
+  (multiple-value-bind (start end)
+      (ppcre:scan *float-scanner* token)
+    (and start end
+         (= (- end start) (length token)))))
+
+(defun is-int (token)
+  "check if the full string is a full floating point number"
+  (multiple-value-bind (start end)
+      (ppcre:scan *int-scanner* token)
+    (and start end
+         (= (- end start) (length token)))))
+
+
     
+(defun tokenize-string (line)
+  (mapcar (lambda (token)
+            (cond ((or (is-float token)
+                       (is-int token))
+                   (read-from-string token))
+                  ((member token '("reflex" "map" "version" "global" "entity" "type" "brush" "vertices" "faces") :test #'string=)
+                   (intern (string-upcase token)))
+                  (t token)))
+          (parse-and-trim line)))
 
 
-(defmethod parse-brush ((self brush) in)
-  (with-slots (vertices faces) self
-    (let ((line
-            (when (if-vertices (read-windows-line in))
-              (loop for line = (read-windows-line in )
-                    while (if-indent line 2)
-                    do
-                       (push (parse-vertex line) vertices)
-                    finally
-                       (setf vertices (nreverse vertices))
-                       (return line)))))
-      (when (if-faces line)
-        (loop for line = (read-windows-line in )
-              while (if-indent line 2)
-              do
-                 (push (parse-face line) faces)
-              finally
-                 (setf faces (nreverse faces))
-                 (return line))))))
+(defun reflex-list-lexer (tokens)
+  #'(lambda ()
+      (let ((value (pop tokens)))
+        (if (null value)
+            (values nil nil)
+            (let ((terminal
+                   (cond ((stringp value) 'string)
+                         ((integerp value) 'integer)
+                         ((floatp value) 'float)
+                         ;; value of symbol is the symbol itself
+                         ((symbolp value) value)
+                         (t (error "Unexpected value ~S" value)))))
+              (values terminal value))))))
 
+
+(defun reflex-map-stream-lexer (in-stream &optional (indent-char #\Tab))
+  (loop with stack = (list 0)
+        with result = nil
+        for line = (read-windows-line in-stream)
+        while line
+        for indent = (count-indent line indent-char)
+        for tokens = (tokenize-string line)
+        for prev-indent = (car stack)
+        if (> indent prev-indent)
+          do
+             (dotimes (i (- indent prev-indent))
+               (push indent stack)
+               (push 'indent result))               
+        if (< indent prev-indent)
+          do
+             (dotimes (i (- prev-indent indent))
+               (push 'dedent result)
+               (pop stack))
+        do
+           ;; (format t "'~a' current indent: ~a prev: ~a~%"
+           ;;         line
+           ;;         indent prev-indent)
+           (dolist (tok tokens)
+             (push tok result))
+           (push 'newline result)
+        finally
+           (dotimes (i (car stack))
+             (push 'dedent result))
+           (return (nreverse result))))
+
+
+(defun reflex-map-lexer (filename)
+  ;; list of tokens:
+  ;; tab, string, newline
+  (with-open-file (in filename :if-does-not-exist nil)
+    (reflex-map-stream-lexer in)))
+
+
+;; grammar:
+;; map ::= header body
+;; header ::= reflex map version integer newline
+;; body ::= global newline indent entries dedent
+;; entries ::= entry newline | entries entry newline
+;; entry ::= brush_entry | entity_entry
+;; entity_entry ::= entity newline indent type string newline entry_attributes dedent
+;; entry_attributes ::= entry_attribute newline | entry_attributes entry_attribute newline
+;; entry_attribute ::= string | entry_attribute string
+
+
+
+(yacc:define-parser *reflex-map-parser*
+  (:start-symbol reflex-map)
+  (:terminals (string integer float newline indent dedent reflex map version global entity type brush vertices faces))
+  (reflex-map (header newline body
+                      (lambda (h n b)
+                        (declare (ignore h n))
+                        (let ((entities-list (remove-if (lambda (x) (eql (type-of x) 'brush)) b))
+                              (brushes-list (remove-if (lambda (x) (eql (type-of x) 'entity)) b)))
+                          (make-instance 'reflex-map :entities entities-list :brushes brushes-list)))))
+
+  (header (reflex map version integer))
+
+  (body (global newline indent entries dedent
+                (lambda (g n i e d)
+                  (declare (ignore g n i d))
+                  e)))
+  (body entries)
+
+  (entries (entry #'list))
+  (entries (entry entries (lambda (e es) (cons e es))))
+
+  (entry entity-entry brush-entry)
+
+  (entity-entry (entity newline indent type string newline entry-attributes dedent
+                        (lambda (e n i type typename n-2 e-a d)
+                          (declare (ignore e n i type n-2 d))
+                          (make-instance 'entity :type typename :properties e-a))))
+
+  (entry-attributes (entry-attribute-line newline
+                                          (lambda (e-a-l nl)
+                                            (declare (ignore nl))
+                                            (list e-a-l))))
   
+  (entry-attributes (entry-attributes entry-attribute-line newline
+                                      (lambda (e-a e-a-l nl)
+                                        (declare (ignore nl))
+                                        (nconc e-a (list e-a-l)))))
+  
+  (entry-attribute-line (entry-attribute-value #'list))
 
-(defmethod parse-next-object ((self reflex-map) line in)
-  (with-slots (entities brushes) self
-    (cond ((if-entity line)
-           (let* ((e (make-instance 'entity))
-                  (next-line (parse-entity e in)))
-             (push e entities)
-             next-line))
-          ((if-brush line)
-           (let* ((b (make-instance 'brush))
-                  (next-line (parse-brush b in)))
-             (push b brushes)
-             next-line))
-          (t (error 'parse-error :text "Unknown field")))))
+  (entry-attribute-line (entry-attribute-value entry-attribute-line
+                                               (lambda (e-a-v e-a-l)
+                                                 (cons e-a-v e-a-l))))
+  (entry-attribute-value string
+                         integer
+                         float)
 
-            
+  (brush-entry (brush newline indent vertices-list faces-list dedent
+                        (lambda (b n i v f d)
+                          (declare (ignore b n i d))
+                          (make-instance 'brush :vertices v :faces f))))
+
+
+  (vertices-list (vertices newline indent vertices-lines dedent
+                           (lambda (v n i v-l d)
+                             (declare (ignore v n i d))
+                             v-l)))
+
+  (vertices-lines (vertex-line newline
+                               (lambda (v n) (list v))))
+  (vertices-lines (vertices-lines vertex-line newline
+                                  (lambda (v-l v n)
+                                    (declare (ignore n))
+                                    (nconc v-l (list v)))))
+  (vertex-line (float float float (lambda (x y z) (make-instance 'vertex :x x :y y :z z))))
+
+  (faces-list (faces newline indent faces-lines dedent
+                     (lambda (f n i f-l d)
+                       (declare (ignore f n i d))
+                       f-l)))
+
+  (faces-lines (face-line newline
+                          (lambda (f-l n)
+                            (declare (ignore n))
+                            (list f-l))))
+                            
+  (faces-lines (faces-lines face-line newline
+                            (lambda (f-l f n)
+                              (declare (ignore n))
+                              (nconc f-l (list f)))))
+                            
+  (face-line (float float float float float integer integer integer string
+                    (lambda (u v s-u s-v rot v1 v2 v3 texture-name)
+                      (make-instance 'face
+                                     :u u :v v :scale-u s-u :scale-v s-v
+                                     :rotation rot
+                                     :texture-name texture-name
+                                     :vertices (list v1 v2 v3)))))
+
+  (face-line (float float float float float integer integer integer integer string
+                    (lambda (u v s-u s-v rot v1 v2 v3 v4 texture-name)
+                      (make-instance 'face
+                                     :u u :v v :scale-u s-u :scale-v s-v
+                                     :rotation rot
+                                     :texture-name texture-name
+                                     :vertices (list v1 v2 v3 v4)))))
+
+  (face-line (float float float float float integer integer integer
+                    (lambda (u v s-u s-v rot v1 v2 v3)
+                      (make-instance 'face
+                                     :u u :v v :scale-u s-u :scale-v s-v
+                                     :rotation rot
+                                     :texture-name ""
+                                     :vertices (list v1 v2 v3)))))
+
+  (face-line (float float float float float integer integer integer integer
+                    (lambda (u v s-u s-v rot v1 v2 v3 v4)
+                      (make-instance 'face
+                                     :u u :v v :scale-u s-u :scale-v s-v
+                                     :rotation rot
+                                     :texture-name ""
+                                     :vertices (list v1 v2 v3 v4))))))
 
 (defun parse-reflex-map-file (filename)
-  (with-open-file (in filename :if-does-not-exist nil)
-    (when-let (ver (parse-header-line in))
-      (let ((m (make-instance 'reflex-map)))
-        (loop while (listen in)
-              with line = (read-windows-line in)
-              do
-                 (setf line (parse-next-object m line in)))
-        (with-slots (brushes entities) m
-          (setf brushes (nreverse brushes)
-                entities (nreverse entities)))
-          m))))
+  (yacc:parse-with-lexer (reflex-list-lexer (reflex-map-lexer filename)) *reflex-map-parser*))
 
 
 (defmethod export-brush ((self brush) out)
@@ -271,113 +357,3 @@
 (defun main(argv)
   (declare (ignore argv))
   (format t "Hello world~%"))
-
-;;; new implementation
-
-(defparameter *stack* (list 0))
-(defparameter *parsed-queue* nil)
-
-(defparameter *float-scanner* 
-  (ppcre:create-scanner "[0-9]+([.][0-9]+([Ee][0-9]+)?)"))
-
-
-(defparameter *int-scanner* 
-  (ppcre:create-scanner "[0-9]+"))
-
-
-(defparameter *input-example-data* "level 1
- level 2
- still level 2
-  level 3
-  also 3
- 2 again
-  another 3
-and back to 1
-another level 2 again
-")
-
-(defparameter *parsed-example-data*
-  '("level" "1"
-    indent "level" "2"
-    "still" "level" "2"
-    indent "level" "3"
-    "also" "3"
-    dedent "2" "again"
-    indent "another" "3"
-    dedent dedent "and" "back" "to" "1"
-    indent "another" "level" "2" "again"
-    dedent))
-
-;; grammar:
-;; simp_stmt = string
-;; stmt = simp_stmt | comp_stmt
-;; comp_stmt = indent stmt dedent
-
-;; map-file = header-line newline rest
-;; rest = (high-level-object)*
-
-;; high-level-object = brush | entity
-;; brush = 
-
-
-(defun tokenize-string (line)
-  (let ((tokens 
-          (parse-and-trim line)))
-    tokens))
-  ;(deflexer test-lexer
-;  ("[0-9]+([.][0-9]+([Ee][0-9]+)?)"
-;    (return (values 'flt (num %0))))
-;  ("[0-9]+"
-;    (return (values 'int (int %0))))
-;  ("[0-9]+X"
-;    (return (values 'int (int %0))))
-;  ("[0-9]+L"
-;    (return (values 'int (int %0))))
-;  ("while" (return 'while))
-;  ("wend" (return 'wend))
-;  ("with" (return 'with))
-;  ("wabbit" (return 'wabbit))
-;  ("for" (return 'for))
-;  ("in" (return 'in))
-;  ("loop" (return 'loop))
-
-
-(defun reflex-map-stream-lexer (in-stream &optional (indent-char #\Tab))
-  (loop for line = (read-windows-line in-stream)
-        while line
-        with result = nil
-        for indent = (count-indent line indent-char)
-        for tokens = (tokenize-string line)
-        for prev-indent = (car *stack*)
-        if (> indent prev-indent)
-          do
-             (dotimes (i (- indent prev-indent))
-               (push indent *stack*)
-               (push 'indent result))               
-        if (< indent prev-indent)
-          do
-             (dotimes (i (- prev-indent indent))
-               (push 'dedent result)
-               (pop *stack*))
-        do
-           ;; (format t "'~a' current indent: ~a prev: ~a~%"
-           ;;         line
-           ;;         indent prev-indent)
-           (dolist (tok tokens)
-             (push tok result))
-           (push 'newline result)
-        finally
-           (dotimes (i (car *stack*))
-             (push 'dedent result))
-           (setf *stack* (list 0))
-           (return (nreverse result))))
-
-
-(defun reflex-map-lexer (filename)
-  ;; list of tokens:
-  ;; tab, string, newline
-  (with-open-file (in filename :if-does-not-exist nil)
-    (reflex-map-stream-lexer in)))
-    
-
-
